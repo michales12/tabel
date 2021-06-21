2f6dad4e4749289d3e752bf48e2aad0131dbb9c4
michales <michal.leskiewcz@outlook.com>
2021-06-21 19:52:36 +0200

    link

--- todo_app/config.py
+++ todo_app/config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = 'b671ea6d27352843fb5225b4713f3f2e'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://pndajxebwsxgpo:bbe3014496fa54a14e2132e75faa0acd4f375db8fc25a6df77d26a7548bf85c2@ec2-108-128-104-50.eu-west-1.compute.amazonaws.com:5432/d4uku8efp4rvbp'
